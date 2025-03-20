c016d416a91e2735d2d26259098c3304be69738a
Ustym Bordun <enderqwer12@gmail.com>
2025-03-20 19:03:30 +0200

    project setup

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,16 @@
+import Container from '../Container/Container';
+import Section from '../Section/Section';
+
+function App() {
+  return (
+    <>
+      <Section>
+        <Container>
+          <p>Goit-react-hw-04</p>
+        </Container>
+      </Section>
+    </>
+  );
+}
+
+export default App;
