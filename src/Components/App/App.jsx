3bed7f84fb639103adfddb4b178b03180ace290b
Ustym Bordun <enderqwer12@gmail.com>
2025-03-21 21:12:11 +0200

    searchBar

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,16 +1,24 @@
-import Container from '../Container/Container';
-import Section from '../Section/Section';
+// import Section from '../Section/Section';
+// import Container from '../Container/Container';
+import SearchBar from '../SearchBar/SearchBar';
 
 function App() {
+  const handleSubmit = value => {
+    console.log(value);
+  };
+
   return (
     <>
-      <Section>
-        <Container>
-          <p>Goit-react-hw-04</p>
-        </Container>
-      </Section>
+      <SearchBar onSubmit={handleSubmit} />
     </>
   );
 }
 
 export default App;
+
+/* 
+https://api.unsplash.com/
+/search/photos
+
+?client_id=WlhDx3QJGuL7onPljCL9sGWrZc0RmZS4gyH8bwfSx9A
+*/
